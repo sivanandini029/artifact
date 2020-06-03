d69b147f3f3e65c5d388f545d9095c5a12e2ccea
Sivanandini B Nair <sivanandinias239@gmail.com>
2020-06-03 23:51:46 +0530

    connect edit profile page to backend

--- js/edit-profile.js
+++ js/edit-profile.js
@@ -4,6 +4,12 @@ const lastNameElem = document.querySelector(".input-group .input-container input
 const bioElem = document.querySelector(".input-container textarea[name=bio]");
 const websiteElem = document.querySelector(".input-group .input-container input[name=website]");
 const dobElem = document.querySelector(".input-group .input-container input[name=dob]");
+const accntCredentialsFormElem = document.querySelector("form[name=account-credentials]");
+const userNameElem = document.querySelector(".input-container input[name=username]");
+const emailElem = document.querySelector(".input-container input[name=email]");
+const basicDetailsErrorElem = document.querySelector("form[name=basic-details] .error");
+const accntCredentialsErrorElem = document.querySelector(".account-credentials .error");
+const disableButton = document.querySelector(".disable-account .button");
 
 fillDetails();
 async function fillDetails() {
@@ -14,18 +20,55 @@ async function fillDetails() {
     bioElem.value = result.bio;
     websiteElem.value = result.website;
     dobElem.value = result.dob;
+    userNameElem.value = result.username;
+    emailElem.value = result.email;
     const dob = new Date(parseInt(result.dob)*1000);
     dobElem.value = `${dob.getDate()}-${dob.getMonth()+1}-${dob.getFullYear()}`;
-}
-    basicDetailsFormElem.addEventListener('submit', async function(e) {
-        e.preventDefault();
-        const firstName = firstNameElem.value;
-        const lastName = lastNameElem.value;
-        const bio = bioElem.value;
-        const website = websiteElem.value;
-        const dob = dobElem.value;
-        await backend.fire("editUser", {first_name:firstName, last_name:lastName, bio:bio, dob:dob, website:website});
-        
-    })
-    
 
+    if(result.status === "DISABLED") 
+    {
+        disableButton.textContent = "Enable";
+    }
+}
+
+basicDetailsFormElem.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const firstName = firstNameElem.value;
+    const lastName = lastNameElem.value;
+    const bio = bioElem.value;
+    const website = websiteElem.value;
+    const dob = dobElem.value;
+    try {
+        basicDetailsErrorElem.textContent = "";
+        await backend.fire("editUser", {first_name:firstName, last_name:lastName, bio:bio, dob:dob, website:website});   
+        basicDetailsErrorElem.textContent = "success";
+    }
+    catch (exception) {
+        basicDetailsErrorElem.innerHTML = exception.replace("/n","<br/>");
+    }
+})
+
+accntCredentialsFormElem.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const username = userNameElem.value;
+    const email = emailElem.value;
+    try {
+        accntCredentialsErrorElem.textContent = "";
+        await backend.fire("editUser", {username:username, email:email});
+        accntCredentialsErrorElem.textContent = "success";
+    }
+    catch(exception) {
+        accntCredentialsErrorElem.innerHTML = exception.replace("/n","<br/>");
+    }
+})
+
+disableButton.addEventListener('click', async function(e) {
+    let status = "ACTIVE";
+
+    if(disableButton.textContent === "Disable") {
+       status = "DISABLED";
+    }
+    console.log(status);
+    await backend.fire("editUser", {status:status});
+    window.location.href = "./edit-profile.html";
+})
