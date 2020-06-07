ac9a45fbaf5d85f21756a2b3afce22c2542e2773
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-06-07 20:21:09 +0530

    Update impressions and correct grammatical errors

--- js/view-article.js
+++ js/view-article.js
@@ -10,24 +10,30 @@ const iconElem = document.querySelector(".labels .label .icon");
 const timeToReadElem = document.querySelector(".meta-article .time-to-read");
 const ownerNameElem2 = document.querySelector(".author-more .name-website .name");
 const bioElem = document.querySelector(".author-more .description");
-const websiteButton2 = document.querySelector(".author-more .name-website button");
+const websiteButton2 = document.querySelector(".author-more .name-website a");
 const followTopicElem = document.querySelector(".follow-container .topic");
 const responseElem = document.querySelector(".response-section .input-container textarea[name=response]");
 const impressionsElem = document.querySelector(".impressions-follow .impressions-container");
-const impressionsNumElem= document.querySelector(".impressions-follow .impressions-container .no-of-impressions");
-const impressionsErrorElem = document.querySelector(".impressions-follow .error");
+const impressionsNumElem = document.querySelector(".impressions-follow .impressions-container .no-of-impressions");
+const impressionsIconElem = document.querySelector(".impressions-container .icon");
 
 fillDetails();
+
 async function fillDetails() {
+    // get info of current user
     const user = await getUser(false, false);
+
+    // get id of the article currently being viewed
     let params = new URLSearchParams(document.location.search.substring(1));
     let id = params.get("id");
-    console.log(id);
-    if(id == null){
+    if(!id){
         window.location.href = "./index.html";
     }
+
+    // get info about the article
     const result = await backend.fire("getArticle", {}, {id});
-    console.log(result);
+    
+
     titleElem.textContent = result.title;
     descriptionElem.textContent = result.description;
     result.content.split("\n").forEach((para) => {
@@ -36,116 +42,53 @@ async function fillDetails() {
         }
     });
     ownerNameElem.textContent = result.owner.username;
-    
     if(result.owner.website) {
-        websiteButton.addEventListener("click",  function() {
-            window.location.href = result.owner.website;
-    
-        });
+        websiteButton.href = window.location.href = result.owner.website;
     } else{
-        websiteButton.style.display = "hidden";
+        websiteButton.style.visibility = "hidden";
     }
     
-    impressionElem.textContent = result.owner.impressions + " " + "impressions" + ", " + result.owner.articles + " " + "articles";
+    setImrpressions(result.owner.articles, result.viewer_has_liked, result.owner.impressions, result.impressions)
     topicElem.textContent = result.topic;
     iconElem.src = "assets/web.svg";
-    timeToReadElem.textContent = Math.floor(contentElem.textContent.replace("\n", "").split(" ").length/100) + " " + "mins" + " " + "read" + " " + "\n\u2022" + " ";
-    const created = new Date(parseInt(result.created)*1000);
-    elem("SPAN", ["time-to-read"], `${months[created.getMonth()]} ${created.getDate()}`, timeToReadElem);
 
+    const timetoReadInt = Math.ceil(contentElem.textContent.replace("\n", "").split(" ").length / 100);
+    const created = new Date(parseInt(result.created)*1000);
+    timeToReadElem.textContent = `${timetoReadInt} min${timetoReadInt > 1? "s": ""} read \n\u2022 ${months[created.getMonth()]} ${created.getDate()}`;
+    
     ownerNameElem2.textContent = result.owner.username;
     bioElem.textContent = result.owner.bio;
 
     if(result.owner.website) {
-        websiteButton2.addEventListener("click",  function() {
-            window.location.href = result.owner.website;
-    
-        });
+        websiteButton2.href = result.owner.website;
     } else{
-        websiteButton2.style.display = "hidden";
+        websiteButton2.style.visibility = "hidden";
     }
 
-    followTopicElem.textContent = "Topic:" + result.topic;
+    followTopicElem.textContent = `Topic: ${result.topic}`;
 
-    
-    impressionsElem.addEventListener("click", async function(e) {
-        e.preventDefault();
+    impressionsElem.addEventListener("click", async function() {
         if(user) {
             try {
-                impressionsErrorElem.textContent = "";
                 const imp = await backend.fire("toggleImpression", {}, {id});
-                impressionsNumElem.textContent = imp.impressions;
+                setImrpressions(imp.owner.articles, imp.viewer_has_liked, imp.owner.impressions, imp.impressions)
             } catch(exception) {
-                    console.log(exception);
+                console.log(exception);
             }
         } else {
-                window.location.href = "./login.html";
-            }
-           
-    
-        });
-       
-    
-    
+            window.location.href = "./login.html";
+        }
+        
+    });    
 }
 
+function setImrpressions(article, viewerLiked, allImpressions, thisImpression) {
+    impressionElem.textContent = `${article} article${article > 1? "s": ""}, ${allImpressions} impression${allImpressions > 1? "s": ""}`;
+    impressionsNumElem.textContent = thisImpression;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // userNameElem.textContent = result.username;
-    // console.log(username);
-    
-    // });
-    // const hai = result.articles;
-    
-    //     
-
-
-
-
+    if (viewerLiked) {
+        impressionsIconElem.classList.add("active");
+    } else {
+        impressionsIconElem.classList.remove("active");
+    }
+}
