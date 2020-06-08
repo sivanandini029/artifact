4fa93b1ca89fb1372f7e8f07d3377d4503cfc43f
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-06-08 10:48:08 +0530

    Remove unnecessary code in change-password backend

--- api/user/change-password.php
+++ api/user/change-password.php
@@ -10,32 +10,7 @@ require_once(__DIR__ ."/../class/User.php");
 session_start();
 $method = (!empty($_SERVER["REQUEST_METHOD"]))? $_SERVER["REQUEST_METHOD"]: "GET";
 
-if ($method === "POST") {
-
-    $req = Request::parse();
-    
-    $user = new User();
-
-    if (($val = $user->register($req)) === false) {
-        Response::send(null, 400, $user->get_error());
-    }
-
-    Response::send($val, 201, "Created new user.");
-
-} else if ($method === "GET" && !empty($_SESSION["username"])) { 
-
-    $username = !empty($_GET["username"])? $_GET["username"]: $_SESSION["username"];
-
-    $user = new User();
-    $user->get_user($username, false, true);
-    
-    if (empty($user->email)) {
-        Response::not_found("user with username '{$username}' not found.");
-    }
-
-    Response::send($user);
-    
-} else if ($method === "PATCH" && !empty($_SESSION["username"])) {
+if ($method === "PATCH" && !empty($_SESSION["username"])) {
     
     $req = Request::parse();
     $user = new User();
