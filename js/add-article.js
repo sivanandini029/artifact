89144a0bd394b91b3969f9e9ba448f4e33fd25bc
Sivanandini B Nair <sivanandinias239@gmail.com>
2020-06-08 20:28:45 +0530

    fill article contents in add article page

--- js/add-article.js
+++ js/add-article.js
@@ -1,5 +1,9 @@
 const formElem = document.querySelector(".form");
 const errorElem = document.querySelector(".form .error");
+const publishButton = document.querySelector(".publish-button button[type=submit]");
+const titleElem = document.querySelector(".input-container input[name=title]");
+const contentElem = document.querySelector(".input-container textarea[name=content]");
+const descriptionElem = document.querySelector(".input-container textarea[name=description]");
 formElem.addEventListener('submit', async function(e) {
     e.preventDefault();
     const title = document.querySelector(".form .input-container input[name=title]").value;
@@ -15,3 +19,20 @@ formElem.addEventListener('submit', async function(e) {
         errorElem.innerHTML = exception.replace("\n","<br/>");
     }
 });
+
+let params = new URLSearchParams(document.location.search.substring(1));
+let id = params.get("id"); 
+if(!id) {
+    publishButton.style.visibility = "hidden";
+ } else {
+        fillArticle();
+        async function fillArticle() {
+        const result = await backend.fire("getArticle", {}, {id});
+        console.log(result);
+        titleElem.value= result.title;
+        descriptionElem.value = result.description;
+        contentElem.value = result.content;
+        }
+}
+    
+    
