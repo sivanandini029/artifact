a522c5de110ec1c503ae497e3d42cfb4a1afef47
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-06-16 20:46:39 +0530

    Add publish and unpublish in add-article

--- js/add-article.js
+++ js/add-article.js
@@ -1,9 +1,13 @@
 const formElem = document.querySelector(".form");
 const errorElem = document.querySelector(".form .error");
-const publishButton = document.querySelector(".publish-button button[type=submit]");
+const publishButton = document.querySelector(".publish-button button");
 const titleElem = document.querySelector(".input-container input[name=title]");
 const contentElem = document.querySelector(".input-container textarea[name=content]");
 const descriptionElem = document.querySelector(".input-container textarea[name=description]");
+let id;
+// load article if id present
+setArticle();
+
 formElem.addEventListener('submit', async function(e) {
     e.preventDefault();
     const title = document.querySelector(".form .input-container input[name=title]").value;
@@ -12,49 +16,60 @@ formElem.addEventListener('submit', async function(e) {
     const content = document.querySelector(".form .input-container textarea[name=content]").value;
     try {
         errorElem.textContent = "";
-        await backend.fire("addArticle", {title, topic, description, content});
-        window.location.href = "./profile.html";
+        if (id) {
+            console.log({title, topic, description, content});
+            await backend.fire("editArticle", {title, topic, description, content}, {id});
+        } else {
+            await backend.fire("addArticle", {title, topic, description, content});
+        }
+        errorElem.textContent = "Saved";
     } catch (exception) {
         console.log(exception);
         errorElem.innerHTML = exception.replace("\n","<br/>");
     }
 });
 
-let params = new URLSearchParams(document.location.search.substring(1));
-let id = params.get("id"); 
-if(!id) {
-    publishButton.style.visibility = "hidden";
- } else {
-        fillArticle();
-        async function fillArticle() {
-        const result = await backend.fire("getArticle", {}, {id});
-        console.log(result);
-        titleElem.value= result.title;
-        descriptionElem.value = result.description;
-        contentElem.value = result.content;
-
+publishButton.addEventListener("click", async function() {
+    try {
+        errorElem.textContent = "";
+        const buttonText = publishButton.textContent;
+        let status = "PUBLISHED";
+        if (buttonText !== "Publish") {
+            status = "SAVED";
+        }        
+        const result = await backend.fire("editArticle", {status}, {id});
         if (result.status === "PUBLISHED") {
             publishButton.textContent = "Unpublish";
+        } else {
+            publishButton.textContent = "Publish";
         }
-        }
-       
-
-        publishButton.addEventListener("click", async function() {
-            await backend.fire("editArticle", {status:status});
-            try {
-                if (publishButton.textContent === "Publish") {
-                    data.status = "PUBLISHED";
-                 } else {
-                     status = "SAVED";
-                 }
-                 console.log(status);
-                 
-                 window.location.href = "./profile.html";
-            } catch (exception) {
-                console.log(exception);
-            }
-            
-        });
-}
+        errorElem.textContent = `${buttonText}ed`;
+    } catch (exception) {
+        console.log(exception);
+    }
     
+});
+
+async function setArticle() {
+    let params = new URLSearchParams(document.location.search.substring(1));
+    id = params.get("id"); 
+    if(!id) {
+        publishButton.style.visibility = "hidden";
+    } else {
+        try {
+            const result = await backend.fire("getArticle", {}, {id});
+            console.log(result);
+            titleElem.value= result.title;
+            descriptionElem.value = result.description;
+            contentElem.value = result.content;
+            if (result.status === "PUBLISHED") {
+                publishButton.textContent = "Unpublish";
+            }
+        } catch (exception) {
+            console.log(exception);
+            window.location.href = "./profile.html"
+        }
+
+    }
+}
     
