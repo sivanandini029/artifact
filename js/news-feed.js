4ea8e7b5f1adb7c329f9e2149da4a267e721e181
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-06-20 01:30:05 +0530

    Changes in news feed
    - Style changes in news feed
    - Load new feed when end of page is reached

--- js/news-feed.js
+++ js/news-feed.js
@@ -1,20 +1,55 @@
 const postContainerElem = document.querySelector(".post-container");
 const userProfileElem = document.querySelector(".user-profile");
 const userArticleElem = document.querySelector(".user-profile .user-articles");
+const emptyMessageElem = document.querySelector(".recent-articles .empty-message");
+let nextPageUrl = 1;
+let requestRunning = false;
+let showedFeeds = [];
 
-getSuggestions();
+initNewsFeed();
+
+async function initNewsFeed() {
+    await getUser(true, false);
+    await getSuggestions();
+}
 
 async function getSuggestions() {
-    await getUser(true, false);
+    if (requestRunning) return false;
     try {
-        const suggestions = await backend.fire("getSuggestions");
-        suggestions.suggestions.forEach(el => {
-            makePost(el);
-        })
+        requestRunning = true;
+        const suggestions = await backend.fire("getSuggestions", {}, {page: nextPageUrl});
+        if (suggestions.next_page) {
+            nextPageUrl = suggestions.next_page;
+            emptyMessageElem.style.display = "none";
+        } else {
+            emptyMessageElem.style.display = "block";
+        }
+        if (suggestions.suggestions.length > 0) {
+            suggestions.suggestions.forEach(el => {
+                if (!showedFeeds.includes(el.id)) {
+                    makePost(el);
+                    showedFeeds.push(el.id);
+                } else {
+                    console.log(el);
+                }
+            })
+        } else if (nextPageUrl === 1) {
+            elem("DIV", ["response", "empty-message"], "No published articles", postContainerElem);
+        }
     } catch (exception) {
-
-    } 
+        console.log(exception)
+    } finally {
+        requestRunning = false;
+    }
 }
+
+window.addEventListener("scroll", async () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        if (nextPageUrl) {
+            getSuggestions();
+        }
+    }
+});
 
 function makePost(data) {
     const postElem = elem("A", ["post"], "", postContainerElem);
@@ -47,18 +82,25 @@ loadProfile();
 
 async function loadProfile() {
     try {
-       const result = await getUser(true, false);
-       console.log(result);
-       const username = document.querySelector(".user-profile .profile-container .profile-name");
-       username.textContent = result.username;
+        const result = await getUser(true, false);
+        const username = document.querySelector(".user-profile .profile-container .profile-name");
+        username.textContent = result.username;
 
-       result.articles.filter(data => data.status === "PUBLISHED").forEach(data => {
-        const postElem = elem("A", ["subject"], "", userArticleElem);
-        postElem.href = `./view-article.html?id=${data.id}`;
+        const userArticles = result.articles.filter(data => data.status === "PUBLISHED").slice(0, 9);
+        if (userArticles.length > 0) {
 
-        const headingElem = elem("DIV", ["heading"], data.title, postElem);
-        elem("DIV", ["readings"], `${data.views} views, ${data.impressions} impressions`, postElem);
-     });
+            userArticles.forEach(data => {
+                const postElem = elem("A", ["subject", "primary-link"], "", userArticleElem);
+                postElem.href = `./view-article.html?id=${data.id}`;
+    
+                elem("DIV", ["heading"], data.title, postElem);
+                elem("DIV", ["readings"], `${data.views} views, ${data.impressions} impressions`, postElem);
+            });
+        } else {
+            const responseElem = elem("DIV", ["response"], "No published articles", userArticleElem);
+            responseElem.style.color = "#999999";
+            responseElem.style.fontSize = "1.3rem";
+        }
     } catch (exception) {
         console.log(exception);
      }
