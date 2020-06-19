83228c73e05fc0ac35f17ef0f613f188b85e2e03
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-06-19 21:24:03 +0530

    Modify header styles

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ const backend = new Backend();
 
 const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+// add navbar as fixed in scroll
+const headerElem = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50 && !headerElem.classList.contains("active")) {
+        headerElem.classList.add("active");
+    } else if (window.scrollY <= 50 && headerElem.classList.contains("active")) {
+        headerElem.classList.remove("active");
+    }
+});
+
 const elem = (type , classNames = [], content = "", parent = document.body, prepend = false) => {
     const el = document.createElement(type);
     classNames.forEach(className => el.classList.add(className));
